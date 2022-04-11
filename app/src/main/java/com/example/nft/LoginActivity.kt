9d07a3d5038a5a4c88ccd34ba3b7c89ebbe90a35
Mejdi97 <mejdi.kchouk@esprit.tn>
2022-04-12 00:16:24 +0200

    commit 1

---
+++ app/src/main/java/com/example/nft/LoginActivity.kt
@@ -0,0 +1,10 @@
+package com.example.nft
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
