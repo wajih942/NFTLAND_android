248f09cc15dee42ad1a615d58659faea16b7755c
wajih942 <wajih.jemai@esprit.tn>
2022-04-18 01:30:01 +0100

    commit

--- app/src/main/java/com/example/nft/DetailsActivity.kt
+++ app/src/main/java/com/example/nft/DetailsActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         var returnbtn : Button = findViewById(R.id.btnreturn)
-        val intent = Intent()
+        var name1 = findViewById<TextView>(R.id.namedetails)
+        var price1 = findViewById<TextView>(R.id.pricedetails)
+        var desc1 = findViewById<TextView>(R.id.descriptiondetails)
+        var seller1 = findViewById<TextView>(R.id.sellerdetails)
+        var image1 = findViewById<TextView>(R.id.image)
+
+
 
         returnbtn.setOnClickListener {
             this.finish()
@@ -19,7 +26,13 @@ class DetailsActivity : AppCompatActivity() {
         val price = intent.getStringExtra("price")
         val description = intent.getStringExtra("desc")
         val seller = intent.getStringExtra("seller")
-        val image = intent.getIntExtra("image",0)
+        val image = intent.getIntExtra("image",1)
+
+        name1.setText(name)
+        price1.setText(price + " ETH")
+        desc1.setText(description)
+        seller1.setText("Seller address : " + seller)
+        image1.
 
     }
 }
